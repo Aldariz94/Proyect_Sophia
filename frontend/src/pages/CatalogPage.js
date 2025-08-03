8d723a9ab19005ef3c07c99487d0c015c1c9f084
Daniel Carreño <danielcv.94@hotmail.com>
2025-08-03 15:58:16 -0400

    reparo CatalogPage.js

--- frontend/src/pages/CatalogPage.js
+++ frontend/src/pages/CatalogPage.js
@@ -157,7 +157,7 @@ const CatalogPage = ({ isUserView = false }) => {
                                 disabled={currentPage === totalPages}
                                 className="px-4 py-2 mx-1 font-medium text-gray-700 bg-gray-200 rounded-md dark:bg-gray-700 dark:text-gray-300 disabled:opacity-50 disabled:cursor-not-allowed"
                             >
-                                Siguiente
+                                Siguientes
                             </button>
                         </div>
                     )}
